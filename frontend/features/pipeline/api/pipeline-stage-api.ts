c0f0e64de59493d85339498969551c4b1a37d854
sushant babu prasai <sushantprasaii7@gmail.com>
2026-06-20 14:27:00 +0545

    Feat: added pipeline stage api methhods

--- frontend/features/pipeline/api/pipeline-stage-api.ts
+++ frontend/features/pipeline/api/pipeline-stage-api.ts
@@ -1,11 +1,59 @@
-import { apiWrapper } from "@/lib/api/api-wrapper";
+import {
+  CreatePipelineStagePayload,
+  CreatePipelineStageResponse,
+  UpdatePipelineStagePayload,
+  UpdatePipelineStageResponse,
+  ReorderPipelineStagesPayload,
+  ReorderPipelineStagesResponse,
+  DeletePipelineStageResponse,
+} from "../types/pipeline-stage.types";
 import { apiURLs } from "@/utils/apiUrls";
-import { CreatePipelineStagePayload } from "../types/pipeline-stage.types";
+import { apiWrapper } from "@/lib/api/api-wrapper";
 
 const pipelineStageApi = {
-  createPipelineStage: async (workspaceId: string, pipelineId: string, pipelineStageData: CreatePipelineStagePayload) => {
-    return await apiWrapper.post(apiURLs.PIPELINE_STAGE.create(workspaceId, pipelineId), pipelineStageData);
+  createPipelineStage: async (
+    workspaceId: string,
+    pipelineId: string,
+    pipelineStageData: CreatePipelineStagePayload,
+  ): Promise<CreatePipelineStageResponse> => {
+    return await apiWrapper.post(
+      apiURLs.PIPELINE_STAGE.create(workspaceId, pipelineId),
+      pipelineStageData,
+    );
   },
-}
+
+  updatePipelineStage: async (
+    workspaceId: string,
+    pipelineId: string,
+    stageId: string,
+    data: UpdatePipelineStagePayload,
+  ): Promise<UpdatePipelineStageResponse> => {
+    return await apiWrapper.patch(
+      apiURLs.PIPELINE_STAGE.update(workspaceId, pipelineId, stageId),
+      data,
+    );
+  },
+
+  reorderPipelineStages: async (
+    workspaceId: string,
+    pipelineId: string,
+    data: ReorderPipelineStagesPayload,
+  ): Promise<ReorderPipelineStagesResponse> => {
+    return await apiWrapper.patch(
+      apiURLs.PIPELINE_STAGE.reorder(workspaceId, pipelineId),
+      data,
+    );
+  },
+
+  deletePipelineStage: async (
+    workspaceId: string,
+    pipelineId: string,
+    stageId: string,
+  ): Promise<DeletePipelineStageResponse> => {
+    return await apiWrapper.delete(
+      apiURLs.PIPELINE_STAGE.delete(workspaceId, pipelineId, stageId),
+    );
+  },
+};
 
 export default pipelineStageApi;
