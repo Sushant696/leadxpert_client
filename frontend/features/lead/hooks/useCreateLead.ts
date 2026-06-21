ac1c2bc5b7e6d218aac4e4715b6925fe189ebc1c
sushant babu prasai <sushantprasaii7@gmail.com>
2026-06-21 19:04:00 +0545

    Feat: create lead hook added

---
+++ frontend/features/lead/hooks/useCreateLead.ts
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { showToast } from "@/components/showToast";
+import { createLeadAction } from "../action/lead-action";
+import { CreateLeadPayload } from "../types/lead-types";
+
+const useCreateLead = (workspaceId: string, pipelineId: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: ["create-lead", workspaceId, pipelineId],
+    mutationFn: (data: CreateLeadPayload) =>
+      createLeadAction(workspaceId, pipelineId, data),
+    onSuccess: () => {
+      showToast.success("Lead created successfully");
+      queryClient.invalidateQueries({
+        queryKey: ["leads", workspaceId, pipelineId],
+      });
+    },
+    onError: (error: Error) => {
+      showToast.error(error.message);
+      console.error("Error creating lead:", error);
+    },
+  });
+};
+
+export default useCreateLead;
